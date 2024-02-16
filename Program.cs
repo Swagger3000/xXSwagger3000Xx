690ad9ecda74efe420f1a450bee0af1c1584195e
Swagger3000 <150146712+Swagger3000@users.noreply.github.com>
2024-02-16 13:37:49 +0200

    Add files via upload
    too

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("foreach nesting");
+
+            int number = 0;
+            var domains = new Dictionary<string, string>()
+            {
+                {"est ","estonia"},
+                {"lat ","latvia"},
+                {"fin ", "finland"},
+                {"swe ", "sweeden"},
+
+            };
+            foreach (var domain in domains)
+            {
+                foreach (var item in domains) ;
+            }
+
+        }
+    }
+} 
